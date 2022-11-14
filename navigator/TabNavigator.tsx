c194aed4f6bf2fe38c4a12e93e2c6c0afed568bf
amansohal8654 <amansohal8654@gmail.com>
2022-11-14 20:56:38 +1300

    done with navigation buttons

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { useNavigation } from '@react-navigation/native';
-import { useLayoutEffect } from 'react';
+import { Icon } from '@rneui/themed';
+import React, { useLayoutEffect } from 'react';
 import CustomersScreen from '../screens/CustomersScreen'
 import OrdersScreen from '../screens/OrdersScreen'
 
@@ -20,7 +21,27 @@ const TabNavigator = () => {
     }, []);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={({route}) =>({
+        tabBarActiveTintColor: "#59c1cc",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({focused, color, size}) => {
+            if(route.name === "Customers"){
+                return (
+                    <Icon
+                    name="user"
+                    type="entypo"
+                    color={focused ? "#59c1cc" : "gray"}
+                    />
+                )
+            } else if(route.name === "Orders") {
+                <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "#EB6A7C" : "gray"}
+                />
+            }
+        }
+    })}>
         <Tab.Screen name="Customers" component={CustomersScreen} />
         <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
