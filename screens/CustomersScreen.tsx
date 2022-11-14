9e52b4a959aa19933d9b39cd43118a555d172a3f
amansohal8654 <amansohal8654@gmail.com>
2022-11-14 21:14:34 +1300

    done with customer ui

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,11 +1,41 @@
-import { Text, SafeAreaView } from 'react-native'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import React, { useLayoutEffect, useState } from 'react';
+import { Text, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native'
 import { useTailwind } from 'tailwind-rn/dist'
+import { RootStackParamList } from '../navigator/RootNavigator';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import {Image, Input} from '@rneui/themed'
 
+export type CustomersScreenNavigationProp = CompositeNavigationProp<
+BottomTabNavigationProp<TabStackParamList, "Customers">,
+NativeStackNavigationProp<RootStackParamList>
+>
 export default function CustomersScreen() {
     const tw = useTailwind()
+    const navigator = useNavigation();
+    const [input, setInput] = useState<string>('')
+
+    useLayoutEffect(() => {
+      navigator.setOptions({
+        headerShown: false,
+      })
+    }, [])
+
   return (
-    <SafeAreaView style={tw("pt-6 pl-2")}>
-      <Text style={tw("text-blue-500")}>CustomersScreen</Text>
-    </SafeAreaView>
+    <ScrollView style={{backgroundColor: '#59c1cc'}}>
+      <Image
+        source={{uri: "https://links.papareact.com/3jc"}}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+
+      <Input 
+        placeholder='Search by Customer' 
+        onChangeText={setInput}
+        containerStyle= {tw("bg-white pt-5 pb-0 px-10")}
+        />
+    </ScrollView>
   )
 }
